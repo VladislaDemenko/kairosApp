a3adc2a873769e4d34c552423ec2b4cefb2319e1
Vladislav Demenko <vvladislademenko@yandex.ru>
2025-12-13 00:58:02 +0300

    k-dAResp-1: Ответ [сервер -> клиент] после усп. аутентификации

---
+++ src/main/java/ru/kairos/dto/AuthResponse.java
@@ -0,0 +1,13 @@
+package ru.kairos.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+    private String email;
+    private String city;
+    private String theme;
+}
