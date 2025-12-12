089dd346e0f6408f52422653d399030d4bcbc836
Vladislav Demenko <vvladislademenko@yandex.ru>
2025-12-13 00:12:17 +0300

    k-mu-1: Написана модель для User

--- src/main/java/ru/kairos/model/User.java
+++ src/main/java/ru/kairos/model/User.java
@@ -1,4 +1,20 @@
 package ru.kairos.model;
 
+import lombok.Data;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
 public class User {
+    private Long id;
+    private String email;
+    private String phone;
+    private String password;
+    private String city;
+    private String theme = "LIGHT"; // Тут еще сможет быть DARK
+    private boolean enabled = true;
+    private LocalDateTime createdAt = LocalDateTime.now();
+    private List<Long> favoritePlaceIds = new ArrayList<>();
+    private List<Long> enabledRandomCategories = new ArrayList<>();
 }
