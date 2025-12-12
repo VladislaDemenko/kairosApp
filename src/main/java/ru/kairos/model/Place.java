5d2bda9c6f2a2a283992445cb5f3e901e6b0e3a1
Vladislav Demenko <vvladislademenko@yandex.ru>
2025-12-13 00:19:28 +0300

    k-mp-1: Написана модель для Place

---
+++ src/main/java/ru/kairos/model/Place.java
@@ -0,0 +1,20 @@
+package ru.kairos.model;
+
+import lombok.Data;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class Place {
+    private Long id;
+    private String name;
+    private String description;
+    private Long categoryId;
+    private String city;
+    private String address;
+    private Double avgCheck;
+    private Boolean hasParking;
+    private Double rating = 0.0;
+    private Integer reviewCount = 0;
+    private List<String> photoUrls = new ArrayList<>();
+}
