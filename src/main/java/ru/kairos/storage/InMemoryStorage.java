a6c16ab683a4aa771de19d51f5147a670bedb198
Vladislav Demenko <vvladislademenko@yandex.ru>
2025-12-13 16:54:07 +0300

    k-sIMS-2: Реализован метод для создания места

--- src/main/java/ru/kairos/storage/InMemoryStorage.java
+++ src/main/java/ru/kairos/storage/InMemoryStorage.java
@@ -26,5 +26,71 @@ public class InMemoryStorage {
     private final AtomicLong slotIdGenerator = new AtomicLong(1);
     private final AtomicLong bookingIdGenerator = new AtomicLong(1);
 
+    private final Map<String, Long> tokenUserId = new ConcurrentHashMap<>();
+    private final Map<Long, String> userIdToToken = new ConcurrentHashMap<>();
 
+    // тестовые данные
+    @PostConstruct
+    public void init() {
+
+    }
+
+    private void initCategories() {
+        List<Category> categoryList = Arrays.asList(
+                new Category(nextCategoryId(), "Кафе"),
+                new Category(nextCategoryId(), "Рестораны"),
+                new Category(nextCategoryId(), "Салоны красоты для женщин"),
+                new Category(nextCategoryId(), "Барбершопы"),
+                new Category(nextCategoryId(), "Музеи"),
+                new Category(nextCategoryId(), "Парки"),
+                new Category(nextCategoryId(), "Здоровый отдых"),
+                new Category(nextCategoryId(), "Активный отдых"),
+                new Category(nextCategoryId(), "Достопримечательности")
+        );
+
+        categoryList.forEach(cat -> categories.put(cat.getId(), cat));
+    }
+
+    // Заведения
+    private void initPlaces() {
+        createMoscowPlaces();
+        createSpbPlaces();
+    }
+
+    private void createMoscowPlaces() {
+
+    }
+
+    private void createSpbPlaces() {
+
+    }
+
+    public void createAndSavePlace(String name, String desc, Long categoryId, String city,
+                                   String address, Double avgCheck, Boolean hasParking, Double rating,
+                                   Integer reviewCount, List<String> photos,
+                                   java.util.function.Consumer<Place> slotInitialize) {
+        Place place = new Place();
+        place.setId(nextPlaceId());
+        place.setName(name);
+        place.setDescription(desc);
+        place.setCategoryId(categoryId);
+        place.setCity(city);
+        place.setAddress(address);
+        place.setAvgCheck(avgCheck);
+        place.setHasParking(hasParking);
+        place.setRating(rating);
+        place.setReviewCount(reviewCount);
+        place.getPhotoUrls().addAll(photos);
+
+        slotInitialize.accept(place);
+        places.put(place.getId(), place);
+    }
+
+    private Long nextCategoryId() {
+        return categoryIdGenerator.getAndIncrement();
+    }
+
+    private Long nextPlaceId() {
+        return placeIdGenerator.getAndIncrement();
+    }
 }
